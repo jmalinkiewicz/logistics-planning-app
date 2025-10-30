7db3576347a95adc68e10965cd72d01ada234a9f
Jakub Malinkiewicz <me@jmalinkiewicz.pl>
2025-10-30 02:39:10 +0100

    initialize spring boot project

---
+++ src/main/java/pl/jmalinkiewicz/logistics_planning_app/LogisticsPlanningAppApplication.java
@@ -0,0 +1,13 @@
+package pl.jmalinkiewicz.logistics_planning_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogisticsPlanningAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogisticsPlanningAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/jmalinkiewicz/logistics_planning_app/LogisticsPlanningAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.jmalinkiewicz.logistics_planning_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisticsPlanningAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
